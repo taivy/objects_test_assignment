f2969c5cdb956c343a3f3a3cb2afb222bf23d379
taivy <aie803ef4g@gmail.com>
2020-10-21 09:02:25 +0300

    basic layout

--- app/src/App.js
+++ app/src/App.js
@@ -5,20 +5,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	    <div className="block-container">
+	    	<div className="block">
+	    		<div className="block-header">
+	    			<div className="block-action">
+	    				
+	    			</div>
+	    		</div>
+	    		<div className="block-media">
+	    			<img src="https://gagadget.com/media/cache/ca/bb/cabb848a7a94d220637c60682e3955ae.jpg" />
+	    		</div>
+	    		<div className="block-content">
+	    			<p>
+	    				Block #1 text
+	    			</p>
+	    		</div>
+	    	</div>
+	    </div>
     </div>
   );
 }
