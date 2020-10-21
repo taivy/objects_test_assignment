e6a191187b23071f5e08668d2c99f03d32f6ea47
taivy <aie803ef4g@gmail.com>
2020-10-21 09:47:42 +0300

    rewritten action btn in material-ui, added simple popover

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,48 @@
 import React from 'react';
+import Popover from '@material-ui/core/Popover';
+import { IconButton } from '@material-ui/core';
 import './App.css';
 
+
 function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleActionBtnClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleActionBtnClose = () => {
+    setAnchorEl(null);
+  };
+
+  const actionPopoverOpen = Boolean(anchorEl);
+  const actionPopoverId = actionPopoverOpen ? 'action-btn-popover' : undefined;
+
   return (
     <div className="App">
 	    <div className="block-container">
 	    	<div className="block">
 	    		<div className="block-header">
 	    			<div className="block-action">
-	    				<button className="action-button">
+	    				<IconButton className="action-button" aria-describedby={actionPopoverId} onClick={handleActionBtnClick}>
 	    					<img src="/dots.svg" />
-	    				</button>
+	    				</IconButton>
+				        <Popover
+				          id={actionPopoverId}
+				          open={actionPopoverOpen}
+				          anchorEl={anchorEl}
+				          onClose={handleActionBtnClose}
+				          anchorOrigin={{
+				            vertical: 'bottom',
+				            horizontal: 'center',
+				          }}
+				          transformOrigin={{
+				            vertical: 'top',
+				            horizontal: 'center',
+				          }}
+				        >
+				          <p>The content of the Popover.</p>
+				        </Popover>
 	    			</div>
 	    		</div>
 	    		<div className="block-media">
